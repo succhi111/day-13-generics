32b809626a0ca579085cb721b0638dbbe11f0160
succhi111 <suchendramishra396@gmail.com>
2022-09-11 10:51:36 +0530

    added:Refactor all code

--- src/FindMaximum.java
+++ src/FindMaximum.java
@@ -1,24 +1,40 @@
 public class FindMaximum
 {
     public static void main(String[] args) {
-        System.out.println("Welcome to Find Maximum Float Problem using Generics");
-        String e = "Apple", f = "Orange" , g = "Mango";    //take variables strings
 
-        //calling directly without creating object bcoz testMaximum method also static.
-        testMaximum (e,f,g);//calling method
+        //1st print welcome msg.
+        System.out.println("Welcome to Generic Program.");
+
+        // variables integer , float , and string
+        Integer a = 89, b = 99, c = 94;
+        Float p = 89.94f, q = 9.99f, r = 29.89f;
+        String e = "Apple", f = "Orange", g = "Mango";
+
+        //calling generic getMethod directly coz in main method and generics method
+        // using static keyword so not needed
+        //to create object for class.
+
+
+        FindMaximum.getMaximum(a, b, c);
+        FindMaximum.getMaximum(p, q, r);
+        FindMaximum.getMaximum(e, f, g);
     }
-    //creating testMaximum method and passing parameter in string type
-    private static void testMaximum(String e, String f, String g) {
-        String max = e; //checking condition comparing and finding max string
-        if(f.compareTo(e)>0){
-            max = f;
+    //create generic method the Generic Type extends Comparable
+
+    private static <T extends Comparable> void getMaximum(T a, T b, T c) {
+
+        //check the condition and finding max for integer string and float
+        T max = a;
+        if (b.compareTo(a) > 0) {
+            max = b;
         }
-        if(g.compareTo(max)>0){
-            max = g;
+        if (c.compareTo(max) > 0) {
+            max = c;
         }
-        System.out.println("Maximum String is: "+max);//print maximum string
+
+        //print maximum no and string
+        System.out.println("Maximum ::" + max);
 
     }
-
 
 }
